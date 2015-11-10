4c629d8f65a53ee9e176b4701ba0f689c4824422
Alexander Pirsig <self@pirsig.net>
2015-11-10 13:48:09 +0100

    decryption adjustments

--- second.js
+++ second.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-let msg = "rcbGvb/NwtLKgIC1ysiE0sva28rR0IzW4amQk7ua2JXq3OTlmvTr8p7goNbG06Tv9fLttartAQGuCP8GsgD1DrYFBw26AgERvggUz8TD8RQUCwkiyhQfzQ/PFyAhF9QZFzDYLSnbLywqNSXhMjg+PzIsO/f4+Q==";
+let msg = "l3fKusi/y8p+0NXQ1siehYi714nf2dDS4OLk0uDXlOze2OyZ7ODf8vDy6fDwo+34sqcB+P+r+QIBA7D3+wUHCbYMBv3/DQ8R/w0EwRQIBxoYGhEYGNnOzQIkFSQWFC3VHyrYGtoiKywi3yQiO+M4NOY6NzVAMOw9Q0lKPTdGAg=="
 let buf = new Buffer(msg, 'base64');
 let offset = 86; // <-- SNEAKY!! makes A apear a in ASCII TABLE
 
@@ -9,7 +9,13 @@ let output = [];
 for (let i = 0; i<buf.length;i++) {
   let chunk = buf[i];
 
-  output.push(String.fromCharCode(((chunk-i)-offset)));
+  let tmpChar = String.fromCharCode(((chunk-i)-offset));
+
+  if (tmpChar.charCodeAt() > 30000) {
+    output.push(String.fromCodePoint((tmpChar.charCodeAt()-(65397-117))));
+  } else {
+    output.push(String.fromCharCode(((chunk-i)-offset)));
+  }
 }
 
 console.log('Result:', output.join(''));
